659531356088601515f16faba26a072e570d2f9d
Hardeep Asrani <hardeepasrani@gmail.com>
2025-02-16 18:24:35 +0800

    add variants

--- library/archive/archive-cover.php
+++ library/archive/archive-cover.php
@@ -7,8 +7,8 @@
  * @since 1.1.0
  */
 
-return '<!-- wp:template-part {"slug":"header","theme":"neve-fse","tagName":"header"} /-->
+return '<!-- wp:template-part {"slug":"header","theme":"church-fse","tagName":"header"} /-->
 
 <!-- wp:pattern {"slug":"neve-fse/templates/archive-cover"} /-->
 
-<!-- wp:template-part {"slug":"footer","theme":"neve-fse","tagName":"footer"} /-->';
+<!-- wp:template-part {"slug":"footer","theme":"church-fse","tagName":"footer"} /-->';
