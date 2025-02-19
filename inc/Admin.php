3c68be75756b36f691b259cd3cf09433d4ec12ff
Hardeep Asrani <hardeepasrani@gmail.com>
2025-02-19 14:16:55 +0800

    chore: disable formbricks for the time being

--- inc/Admin.php
+++ inc/Admin.php
@@ -62,7 +62,7 @@ class Admin {
 		add_action( 'admin_notices', array( $this, 'render_survey_notice' ) );
 		add_action( 'wp_ajax_church_fse_dismiss_welcome_notice', array( $this, 'remove_welcome_notice' ) );
 		add_action( 'wp_ajax_church_fse_dismiss_survey_notice', array( $this, 'remove_survey_notice' ) );
-		add_action( 'admin_print_scripts', array( $this, 'add_nps_form' ) );
+		// add_action( 'admin_print_scripts', array( $this, 'add_nps_form' ) ); Disable for now
 
 		add_action( 'wp_ajax_church_fse_set_otter_ref', array( $this, 'set_otter_ref' ) );
 	}
