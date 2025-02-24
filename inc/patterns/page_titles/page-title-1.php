504ce4db78ab4b940ec1b31fba5a0dfc17d86650
Hardeep Asrani <hardeepasrani@gmail.com>
2025-02-24 14:30:39 +0800

    feat: add patterns (#10)

--- inc/patterns/page_titles/page-title-1.php
+++ inc/patterns/page_titles/page-title-1.php
@@ -7,18 +7,22 @@
  * @since 1.0.0
  *
  * slug: page-title-1
- * title: Page Title - Simple
+ * title: Page Title
  * categories: ChurchFSE
- * keywords: Page Title, Post title
+ * keywords: page title
  */
 
+use ChurchFSE\Assets_Manager;
+
 return array(
-	'title'      => __( 'Page Title - Simple', 'church-fse' ),
+	'title'      => __( 'Page Title', 'church-fse' ),
 	'categories' => array( 'church-fse', 'posts' ),
-	'keywords'   => array( 'Page Title', 'Post title' ),
+	'keywords'   => array( 'page title' ),
 	'content'    => '
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"8px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"8px"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:8px"><!-- wp:post-title {"textAlign":"center","level":1} /--></div>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"8px"},"elements":{"link":{"color":{"text":"var:preset|color|ti-fg-alt"}}}},"backgroundColor":"ti-bg-inv","textColor":"ti-fg-alt","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-ti-fg-alt-color has-ti-bg-inv-background-color has-text-color has-background has-link-color" style="margin-top:0px;margin-bottom:0px;padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:heading {"textAlign":"center","level":1,"align":"wide"} -->
+<h1 class="wp-block-heading alignwide has-text-align-center">Title goes here</h1>
+<!-- /wp:heading --></div>
 <!-- /wp:group -->
         ',
 );
--- inc/patterns/page_titles/page-title-2.php
+++ inc/patterns/page_titles/page-title-2.php
@@ -7,22 +7,26 @@
  * @since 1.0.0
  *
  * slug: page-title-2
- * title: Page Title - Dark
+ * title: Page Title
  * categories: ChurchFSE
- * keywords: Page Title, Post title, background, inverted, dark
+ * keywords: page title, cover
  */
 
+use ChurchFSE\Assets_Manager;
+
+$church_fse_img04 = Assets_Manager::get_image_url( 'church-fse-4.webp' );
+
 return array(
-	'title'      => __( 'Page Title - Dark', 'church-fse' ),
+	'title'      => __( 'Page Title', 'church-fse' ),
 	'categories' => array( 'church-fse', 'posts' ),
-	'keywords'   => array( 'Page Title', 'Post title', 'background', 'inverted', 'dark' ),
+	'keywords'   => array( 'page title', 'cover' ),
 	'content'    => '
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"64px"},"blockGap":"8px","margin":{"top":"0px","bottom":"0px"}}},"backgroundColor":"ti-bg-inv","textColor":"ti-bg-alt","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-ti-bg-alt-color has-ti-bg-inv-background-color has-text-color has-background" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:64px"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide"><!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide"><!-- wp:post-title {"textAlign":"center","level":1,"align":"wide"} /--></div>
-<!-- /wp:group --></div>
-<!-- /wp:group --></div>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:cover {"url":"' . esc_url( $church_fse_img04 ) . '","id":299,"dimRatio":90,"overlayColor":"ti-bg-inv","isUserOverlayColor":true,"minHeight":300,"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-cover alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40);min-height:300px"><span aria-hidden="true" class="wp-block-cover__background has-ti-bg-inv-background-color has-background-dim-90 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-299" alt="" src="' . esc_url( $church_fse_img04 ) . '" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"align":"wide"} -->
+<h1 class="wp-block-heading alignwide has-text-align-center">Title goes here</h1>
+<!-- /wp:heading --></div></div>
+<!-- /wp:cover --></div>
 <!-- /wp:group -->
         ',
 );
